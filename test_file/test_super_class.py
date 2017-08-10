eae1b699668a8795cb4b02fb28cf5e7f55517765
linchengzeng <87335610@qq.com>
2017-08-10 18:05:32 +0800

    2017年8月10日 下班

---
+++ test_file/test_super_class.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+#  Author:aling
+
+class School():
+    def __init__(self, ID, school_name, school_addr, school_tel):
+        self.ID = ID
+        self.School_Name = school_name
+        self.School_Addr = school_addr
+        self.School_Tel = school_tel
+
+class Course():
+    def __init__(self, ID, course_name, course_period, course_cost, School):
+        self.ID = ID
+        self.Course_name = course_name
+        self.Course_period = course_period
+        self.Course_cost = course_cost
+        self.Course_School = School
+
+#**********************************
+
+class School():
+    def __init__(self, ID, school_name):
+        self.ID = ID
+        self.School_Name = school_name
+
+    def add_course(self,course_id,course_name, course_period, course_cost, School):
+        result = Course(course_id,course_name, course_period, course_cost, School)
+
+class Course():
+    def __init__(self, ID, course_name, course_period, course_cost, School):
+        self.ID = ID
+        self.Course_name = course_name
+        self.Course_period = course_period
+        self.Course_cost = course_cost
+        self.Course_School = School
