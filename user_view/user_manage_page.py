5f8bf2b4128eaeafddb6e0c6eeaffbc05f6b1a4a
linchengzeng <87335610@qq.com>
2017-08-21 21:57:22 +0800

    2017年8月20日 结束

--- user_view/user_manage_page.py
+++ user_view/user_manage_page.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 #  Author:aling
 
-from information_operator import edit_obj_infomation
+from information_operator import edit_obj_infomation,update_obj_infomation
 import style,main_master
 
 class User_info_manage():
@@ -20,13 +20,18 @@ class User_info_manage():
             print(style.user_view_menu_desc)
             user_select_menu = input('\033[31;1m请选择您需要的操作>>：\033[0m')
             if user_select_menu in style.user_view_menu:
-                result = user_menu[user_select_menu](user_data['account_data'],'student_manage')
+                result = user_menu[user_select_menu](user_data['account_data'], 'student_manage')
                 user_data['account_data'] = result
             else:
                 print('您选择的不在我提供的服务范围内，请重新选择！')
 
-    def edit_pwd(self):
-        print(self.Pwd)
+    def edit_pwd(self,obj_table):
+        old_pwd = input('\033[31;1m请输入您的原密码>>：\033[0m')
+        new_pwd = input('\033[31;1m请输入您的新密码>>：\033[0m')
+        if self.Pwd == old_pwd:
+            result = update_obj_infomation.uptate_info.update_student_obj(obj_table, 'Pwd', new_pwd, self)
+        if result:
+            print('\033[30;1m修改成功！\033[0m')
 
     def u_info_manage(self, obj_table):
         result = edit_obj_infomation.Editor_info.edit_student_obj(obj_table, self)
